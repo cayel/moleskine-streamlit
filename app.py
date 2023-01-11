bf8219cdfe51c4fc65f0be8881711380c57b525b
Christophe Ayel <christophe.ayel@gmail.com>
2023-01-11 22:41:13 +0100

    authentication

--- app.py
+++ app.py
@@ -1,3 +1,28 @@
 import streamlit as st
+import streamlit_authenticator as stauth
+import yaml
+import toml
 
 st.title('Moleskine')
+
+with open('.streamlit/config.toml') as file:
+    config = toml.load(file)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+
+name, authentication_status, username = authenticator.login('Login', 'main')
+
+if st.session_state["authentication_status"]:
+    authenticator.logout('Logout', 'main')
+    st.write(f'Welcome *{st.session_state["name"]}*')
+    st.title('Some content')
+elif st.session_state["authentication_status"] == False:
+    st.error('Username/password is incorrect')
+elif st.session_state["authentication_status"] == None:
+    st.warning('Please enter your username and password')
